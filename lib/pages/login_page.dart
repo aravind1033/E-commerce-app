5efc104252231d40b5131e7474265e7c8987b895
macbookpro <aravindkarthika1999@gmail.com>
2024-02-05 22:57:02 +0530

    Day 2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final TextEditingController phoneNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Welcome Back!")
-        ],
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Welcome Back!"),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              keyboardType: TextInputType.phone,
+              controller: phoneNumberController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Phone Number',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print("onpressed");
+                },
+                child: const Text("Login")),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+                onTap: () {}, child: const Text("Register New Account")),
+          ],
+        ),
       ),
     );
   }
 }
-
